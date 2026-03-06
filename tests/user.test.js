f75d2c16f8744cdb4974b7a81e3eb8eab6ff30d4
annasnurdin <anasnurdin02@gmail.com>
2026-03-06 10:22:23 +0700

    testing validation

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,6 +1,7 @@
 import supertest from "supertest"
 import { prismaClient } from "../src/application/database.js"
 import { web } from "../src/application/web.js"
+import { logger } from "../src/application/logging.js"
 
 describe('POST /api/users', function () {
     afterEach(async () => {
@@ -23,5 +24,20 @@ describe('POST /api/users', function () {
         expect(result.body.data.username).toBe("annas")
         expect(result.body.data.name).toBe("Annas Nurdin")
         expect(result.body.data.password).toBeUndefined()
+    }),
+    it('should reject if user invalid', async () => {
+        const result = await supertest(web)
+            .post('/api/users')
+            .send({
+                username: "",
+                password: "",
+                name: ""
+            })
+        
+        logger.info(result.body)
+
+        expect(result.status).toBe(400)
+        expect(result.body.errors).toBeDefined()
+        
     })
 })
